555f43bca7f83483f3c9b95643feaba9be4511e8
Ryan <leehahnwoong@gmail.com>
2024-02-06 16:02:12 -0800

    fix: switch database to sqlite for dev

--- backend/healthmate/settings.py
+++ backend/healthmate/settings.py
@@ -76,15 +76,22 @@ WSGI_APPLICATION = 'healthmate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django_prometheus.db.backends.postgresql',
+#         'NAME': 'healthmate',
+#         'USER': 'team62',
+#         'PASSWORD': 'team62',
+#         # 'HOST': 'sqldb',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django_prometheus.db.backends.postgresql',
-        'NAME': 'healthmate',
-        'USER': 'team62',
-        'PASSWORD': 'team62',
-        # 'HOST': 'sqldb',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "healthmate",
     }
 }
 
