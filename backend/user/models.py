17f4e99488ae46d301c8d4a4449f42eb8e9e5e89
claudiosuccar <claudiosuccar@gmail.com>
2024-02-22 13:26:11 -0800

    Fix: Removed confirm email and confirm password fields from models.py and serializers.py

--- backend/user/models.py
+++ backend/user/models.py
@@ -2,6 +2,5 @@ from django.db import models
 
 class User(models.Model):
     email = models.EmailField(unique=True)
-    confirm_email = models.EmailField()
     password = models.CharField(max_length=128)
-    confirm_password = models.CharField(max_length=128)
+    
--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'confirm_email', 'password', 'confirm_password']
+        fields = ['email', 'password']
