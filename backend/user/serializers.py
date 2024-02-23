ae9cf964e075f524455118e12a10e673b95eb634
claudiosuccar <claudiosuccar@gmail.com>
2024-02-23 09:26:06 -0800

    Feat: Implemented LogoutSerializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -30,3 +30,16 @@ class LoginSerializer(serializers.Serializer):
             raise serializers.ValidationError('Invalid email or password.')
 
         return user
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+
+    def save(self, **kwargs):
+        try:
+            RefreshToken(self.token).blacklist()
+        except TokenError:
+            self.fail('bad_token')
