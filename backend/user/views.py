94625e3ae0a0bb8a664c14fd34a3856f5cb22897
claudiosuccar <claudiosuccar@gmail.com>
2024-02-23 09:32:36 -0800

    Feat: Implemented Logout view

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,4 +1,3 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics,status,views,permissions
 from .serializers import SignupSerializer,LoginSerializer,LogoutSerializer
@@ -26,3 +25,12 @@ class LoginAPIView(generics.GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+class LogoutAPIView(generics.GenericAPIView):
+    serializer_class = LogoutSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
