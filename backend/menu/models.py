4328e60103fa4ef4b2d25e45aecd8fe86f373112
Ryan <leehahnwoong@gmail.com>
2024-02-22 16:59:53 -0800

    fix: model for menu item

--- backend/menu/models.py
+++ backend/menu/models.py
@@ -3,10 +3,9 @@ from django.db import models
 class Menu(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField()
+    day = models.IntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     period = models.CharField(max_length=128)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
