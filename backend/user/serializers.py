d208e081cd0e9ff2f0cb4358900f3dc3217a8be9
claudiosuccar <claudiosuccar@gmail.com>
2024-02-23 09:24:26 -0800

    Fix: Specified UserSerializer as SignupSerializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
+class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'password']
