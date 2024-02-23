20da42992c7e6b212f8302907531f7d442323cb8
claudiosuccar <claudiosuccar@gmail.com>
2024-02-23 09:30:21 -0800

    Feat: Implemented Login view

--- backend/user/views.py
+++ backend/user/views.py
@@ -19,3 +19,10 @@ class Signup(generics.GenericAPIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
