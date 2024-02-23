a9bb9a2035976d1d19c08d5ea14f57b4c69f36f2
claudiosuccar <claudiosuccar@gmail.com>
2024-02-23 09:34:27 -0800

    Feat: Updated urls.py

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+app_name = 'user'
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('login/',views.LoginAPIView.as_view(),name="login"),
+    path('logout/', views.LogoutAPIView.as_view(), name="logout"),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
