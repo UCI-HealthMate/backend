02a714c83c4adc982a33635f64092a18fd0cc88c
claudiosuccar <claudiosuccar@gmail.com>
2024-02-23 09:25:16 -0800

    Feat: Implemented LoginSerializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -7,3 +7,26 @@ class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'password']
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    tokens = serializers.SerializerMethodField()
+
+    def get_tokens(self, obj):
+        user = obj
+        refresh = RefreshToken.for_user(user)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+        user = authenticate(email=email, password=password)
+
+        if not user:
+            raise serializers.ValidationError('Invalid email or password.')
+
+        return user
