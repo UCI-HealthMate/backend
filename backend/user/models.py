efd142b8f40bd0d2ad39f8928d2a9506ee144a67
claudiosuccar <claudiosuccar@gmail.com>
2024-02-23 09:22:01 -0800

    Feat: Extended the User model to create tokens

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.utils import timezone
 
 class User(models.Model):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
     password = models.CharField(max_length=128)
-    
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return{
+            'refresh':str(refresh),
+            'access':str(refresh.access_token)
+        }
