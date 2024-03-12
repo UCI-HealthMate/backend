34c4b4bcc56678bccb071a04673a54f5365fd90e
Ryan <leehahnwoong@gmail.com>
2024-03-12 14:34:00 -0700

    feat: Add menu serializer and view

--- backend/menu/serializer.py
+++ backend/menu/serializer.py
@@ -5,3 +5,22 @@ class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = ['name', 'description', 'day', 'price', 'period', 'date']
+
+class MenuRequestSerializer(serializers.Serializer):
+    Calories = serializers.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    ContainsEggs = serializers.BooleanField(default=False)
+    ContainsFish = serializers.BooleanField(default=False)
+    ContainsMilk = serializers.BooleanField(default=False)
+    ContainsPeanuts = serializers.BooleanField(default=False)
+    ContainsSesame = serializers.BooleanField(default=False)
+    ContainsShellfish = serializers.BooleanField(default=False)
+    ContainsSoy = serializers.BooleanField(default=False)
+    ContainsTreeNuts = serializers.BooleanField(default=False)
+    ContainsWheat = serializers.BooleanField(default=False)
+    IsGlutenFree = serializers.BooleanField(default=False)
+    IsHalal = serializers.BooleanField(default=False)
+    IsKosher = serializers.BooleanField(default=False)
+    IsLocallyGrown = serializers.BooleanField(default=False)
+    IsOrganic = serializers.BooleanField(default=False)
+    IsVegan = serializers.BooleanField(default=False)
+    IsVegetarian = serializers.BooleanField(default=False)
--- backend/menu/views.py
+++ backend/menu/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import JSONRenderer
+# from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework import status
-from django.utils import timezone
+# from django.utils import timezone
 from menu.task import fetch_and_update_menu
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework_simplejwt.tokens import AccessToken
 
 class Items(APIView):
     @swagger_auto_schema(
@@ -14,14 +15,29 @@ class Items(APIView):
         responses={200: 'Menu fetched successfully', 400: 'Bad Request'}
     )
     def get(self, request):
-        pass
+        access_token = request.COOKIES.get('access_token')
+        if not access_token:
+            return Response({'error': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+        token = AccessToken(access_token)
+        user_email = token['email']
+        if user_email is None:
+            return Response({'error': 'Invalid Token'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({'message': f'user email for testing: {user_email}'}, status=status.HTTP_200_OK)
+        
     
     @swagger_auto_schema(
         operation_description="Update User Menu",
         responses={200: 'Menu updated successfully', 400: 'Bad Request'}
     )
     def post(self, request):
-        pass
+        access_token = request.COOKIES.get('access_token')
+        if not access_token:
+            return Response({'error': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+        token = AccessToken(access_token)
+        user_email = token['email']
+        if user_email is None:
+            return Response({'error': 'Invalid Token'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({'message': f'user email for testing: {user_email}'}, status=status.HTTP_200_OK)
 
 class Trigger(APIView):
     @swagger_auto_schema(
