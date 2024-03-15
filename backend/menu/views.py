d6078c8ad1938827d0ad02fb0b31efe0b0d5b17a
Ryan <leehahnwoong@gmail.com>
2024-03-15 13:56:55 -0700

    fix: remove post method on items

--- backend/menu/views.py
+++ backend/menu/views.py
@@ -93,24 +93,24 @@ class Items(APIView):
         return Response(menu_dict, status=status.HTTP_200_OK)
 
 
-    @swagger_auto_schema(
-        operation_description="Update User Menu",
-        responses={200: 'Menu updated successfully', 400: 'Bad Request'}
-    )
-    def post(self, request):
-        access_token = request.COOKIES.get('access_token')
-        if not access_token:
-            return Response({'error': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
-        token = AccessToken(access_token)
-        token_data = token.payload
-        user_id = token_data.get('user_id')
-        try:
-            user = User.objects.get(id=user_id)
-        except User.DoesNotExist:
-            return None
-        if user_id is None:
-            return Response({'error': 'Invalid Token'}, status=status.HTTP_401_UNAUTHORIZED)
-        return Response({'message': f'user email for testing: {user.email}'}, status=status.HTTP_200_OK)
+    # @swagger_auto_schema(
+    #     operation_description="Update User Menu",
+    #     responses={200: 'Menu updated successfully', 400: 'Bad Request'}
+    # )
+    # def post(self, request):
+    #     access_token = request.COOKIES.get('access_token')
+    #     if not access_token:
+    #         return Response({'error': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+    #     token = AccessToken(access_token)
+    #     token_data = token.payload
+    #     user_id = token_data.get('user_id')
+    #     try:
+    #         user = User.objects.get(id=user_id)
+    #     except User.DoesNotExist:
+    #         return None
+    #     if user_id is None:
+    #         return Response({'error': 'Invalid Token'}, status=status.HTTP_401_UNAUTHORIZED)
+    #     return Response({'message': f'user email for testing: {user.email}'}, status=status.HTTP_200_OK)
 
 class Trigger(APIView):
     @swagger_auto_schema(
